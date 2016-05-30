c3dcb6233a59c60044b74aaa732666fb5f78b82b
Frank Carver <frank.carver@conga.io>
2016-05-30 15:56:21 +0100

    case placeholders for the rest of the MM57109 opcodes

--- mm57109.c
+++ mm57109.c
@@ -20,6 +20,7 @@ float mm57109_get_register(struct MM57109_register* reg) {
 
 void mm57109_op(struct MM57109* mm, uint8_t op) {
 	switch(op) {
+
 	case OP_0: case OP_1: case OP_2: case OP_3: case OP_4: case OP_5: case OP_6: case OP_7: case OP_8: case OP_9:
 		switch(mm->state) {
 		case integer_entry:
@@ -60,6 +61,154 @@ void mm57109_op(struct MM57109* mm, uint8_t op) {
 	case OP_HALT:
 		//TODO AFAICT this doesn't actually change any states or values
 	break;
+
+	case OP_TJC:
+		//TODO
+	break;
+	case OP_TX0:
+		//TODO
+	break;
+	case OP_TXLT0:
+		//TODO
+	break;
+	case OP_TXF:
+		//TODO
+	break;
+	case OP_TERR:
+		//TODO
+	break;
+	case OP_JMP:
+		//TODO
+	break;
+	case OP_OUT:
+		//TODO
+	break;
+	case OP_IN:
+		//TODO
+	break;
+	case OP_SMDC:
+		//TODO
+	break;
+	case OP_IBNZ:
+		//TODO
+	break;
+	case OP_DBNZ:
+		//TODO
+	break;
+	case OP_XEM:
+		//TODO
+	break;
+	case OP_MS:
+		//TODO
+	break;
+	case OP_MR:
+		//TODO
+	break;
+	case OP_LSH:
+		//TODO
+	break;
+	case OP_RSH:
+		//TODO
+	break;
+
+	case OP_INV:
+		//TODO
+	break;
+	case OP_EM:
+		//TODO
+	break;
+	case OP_TOGM:
+		//TODO
+	break;
+	case OP_ROLL:
+		//TODO
+	break;
+	case OP_SIN:
+		//TODO
+	break;
+	case OP_COS:
+		//TODO
+	break;
+	case OP_TAN:
+		//TODO
+	break;
+	case OP_SF1:
+		//TODO
+	break;
+	case OP_PF1:
+		//TODO
+	break;
+	case OP_SF2:
+		//TODO
+	break;
+	case OP_PF2:
+		//TODO
+	break;
+	case OP_ECLR:
+		//TODO
+	break;
+	case OP_RTD:
+		//TODO
+	break;
+	case OP_DTR:
+		//TODO
+	break;
+	case OP_POP:
+		//TODO
+	break;
+	case OP_MCLR:
+		//TODO
+	break;
+
+	case OP_XEY:
+		//TODO
+	break;
+	case OP_EX:
+		//TODO
+	break;
+	case OP_10X:
+		//TODO
+	break;
+	case OP_SQ:
+		//TODO
+	break;
+	case OP_SQRT:
+		//TODO
+	break;
+	case OP_LN:
+		//TODO
+	break;
+	case OP_LOG:
+		//TODO
+	break;
+	case OP_1_X:
+		//TODO
+	break;
+	case OP_YX:
+		//TODO
+	break;
+	case OP_PLUS:
+		//TODO
+	break;
+	case OP_MINUS:
+		//TODO
+	break;
+	case OP_MUL:
+		//TODO
+	break;
+	case OP_DIV:
+		//TODO
+	break;
+	case OP_PRW1:
+		//TODO
+	break;
+	case OP_PRW2:
+		//TODO
+	break;
+	case OP_NOP:
+		//TODO
+	break;
+
 	}
 }
 
